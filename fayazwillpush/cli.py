a71de868742fad566a1ee470c430c7763b3c2996
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2026-06-07 03:36:11 +0600

    adding new thing

--- fayazwillpush/cli.py
+++ fayazwillpush/cli.py
@@ -1,31 +1,50 @@
 import subprocess
 import typer
+import json
+import os
 
 app = typer.Typer()
 
+CONFIG_FILE = os.path.expanduser("~/.fayazpush.json")
+
+
+def save_config(repo_url):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump({"repo": repo_url}, f)
+
+
+def load_config():
+    if not os.path.exists(CONFIG_FILE):
+        return None
+    with open(CONFIG_FILE, "r") as f:
+        return json.load(f)
+
+
 @app.command()
-def push(
-    repo_url: str,
-    message: str = "Initial Commit"
-):
+def init(repo_url: str):
+
+    save_config(repo_url)
+
+    subprocess.run(["git", "init"])
+    subprocess.run(["git", "branch", "-M", "main"])
+
+    subprocess.run(["git", "remote", "remove", "origin"], stderr=subprocess.DEVNULL)
+    subprocess.run(["git", "remote", "add", "origin", repo_url])
+
+    print("✓ Repo initialized and saved!")
 
 
-    commands = [
-        ["git", "init"],
-        ["git", "add", "."],
-        ["git", "commit", "-m", "Auto commit"],
-        ["git", "branch", "-M", "main"],
-        ["git", "remote", "remove", "origin"],
-        ["git", "remote", "add", "origin", repo_url],
-        ["git", "push", "-u", "origin", "main"]
-    ]
+@app.command()
+def push(message: str = "Auto commit"):
 
-    for cmd in commands:
-        try:
-            subprocess.run(cmd, check=True)
-            print("✓", " ".join(cmd))
-        except:
-            pass
+    config = load_config()
 
-if __name__ == "__main__":
-    app()
+    if not config:
+        print("No repo found. Run: fayazpush init <repo_url>")
+        return
+
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", message])
+    subprocess.run(["git", "push"])
+
+    print("Fayaz Pushed it successfully!")
