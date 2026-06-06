225ed8b5b5723a93c9ad9aa1e3fa2ee48acf7902
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2026-06-07 02:56:19 +0600

    Auto commit

---
+++ fayazwillpush/cli.py
@@ -0,0 +1,27 @@
+import subprocess
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def push(repo_url: str):
+
+    commands = [
+        ["git", "init"],
+        ["git", "add", "."],
+        ["git", "commit", "-m", "Auto commit"],
+        ["git", "branch", "-M", "main"],
+        ["git", "remote", "remove", "origin"],
+        ["git", "remote", "add", "origin", repo_url],
+        ["git", "push", "-u", "origin", "main"]
+    ]
+
+    for cmd in commands:
+        try:
+            subprocess.run(cmd, check=True)
+            print("✓", " ".join(cmd))
+        except:
+            pass
+
+if __name__ == "__main__":
+    app()
